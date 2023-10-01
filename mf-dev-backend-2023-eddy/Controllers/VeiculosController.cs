29b54818cf01abf61256454845b0606091d63922
Eddy Moura <mouragestao@gmail.com>
2023-10-01 19:32:43 -0300

    Create Veiculos Controller and Update Desing dependecy

---
+++ mf-dev-backend-2023-eddy/Controllers/VeiculosController.cs
@@ -0,0 +1,22 @@
+﻿using mf_dev_backend_2023_eddy.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace mf_dev_backend_2023_eddy.Controllers
+{
+    public class VeiculosController : Controller
+    {
+        private readonly AppDbContext _context;
+        public VeiculosController(AppDbContext context) 
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index ()
+        {
+            var dados = await _context.Veiculos.ToListAsync();
+
+            return View(dados);
+        }
+    }
+}

5b725ea97d810f6da8363fa0660453cb780e6459
Eddy Moura <mouragestao@gmail.com>
2023-10-06 07:26:50 -0300

    -Create user model, controller and view

--- mf-dev-backend-2023-eddy/Models/AppDbContext.cs
+++ mf-dev-backend-2023-eddy/Models/AppDbContext.cs
@@ -8,6 +8,9 @@ namespace mf_dev_backend_2023_eddy.Models
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
         public DbSet<Veiculo> Veiculos { get; set; }
+
         public DbSet<Consumo> Consumos { get; set; }
+
+        public DbSet<Usuario> Usuarios { get; set; }
     }
 }
